d4a32e4b09d10736729f82c14db943c638adcbc3
christopher-villarin <christopher.villarin@hogarth.com>
2026-03-24 15:33:48 +0800

    changing colors / design / added savings

--- src/layout/BottomNavBar.tsx
+++ src/layout/BottomNavBar.tsx
@@ -1,32 +1,43 @@
 import { NavLink } from "react-router-dom";
+import {
+  HomeIcon,
+  BanknotesIcon,
+   BuildingLibraryIcon,
+  CurrencyDollarIcon,
+  ReceiptPercentIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/outline";
 
 export default function BottomNavBar() {
   const navItems = [
-    { name: "Home", path: "/" },
-    { name: "Loans", path: "/loans" },
-    { name: "Savings", path: "/savings" },
-    { name: "Salary", path: "/salary" },
-    { name: "Bills", path: "/bills" },
-    { name: "Notes", path: "/notes" },
-
+    { name: "Home", path: "/", icon: HomeIcon },
+    { name: "Loans", path: "/loans", icon: BanknotesIcon },
+    { name: "Savings", path: "/savings", icon: BuildingLibraryIcon },
+    { name: "Salary", path: "/salary", icon: CurrencyDollarIcon },
+    { name: "Bills", path: "/bills", icon: ReceiptPercentIcon },
+    { name: "Notes", path: "/notes", icon: DocumentTextIcon },
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-black border-t border-mist-900 flex justify-around py-6 z-50 px-4">
-      {navItems.map((item) => (
-        <NavLink
-          key={item.name}
-          to={item.path}
-          className={({ isActive }) =>
-            `flex flex-col items-center text-sm ${
-              isActive ? "text-[#CEE36E]" : "text-gray-400"
-            }`
-          }
-        >
-          {/* You can replace the span with icons if you want */}
-          <span className="font-semibold">{item.name}</span>
-        </NavLink>
-      ))}
+    <nav className="fixed bottom-0 left-0 w-full bg-black border-t border-mist-900 flex justify-around py-3 z-50 px-4">
+      {navItems.map((item) => {
+        const Icon = item.icon;
+
+        return (
+          <NavLink
+            key={item.name}
+            to={item.path}
+            className={({ isActive }) =>
+              `flex flex-col items-center text-xs ${
+                isActive ? "text-[#CEE36E]" : "text-gray-400"
+              }`
+            }
+          >
+            <Icon className="w-6 h-6 mb-1" />
+            <span className="font-semibold">{item.name}</span>
+          </NavLink>
+        );
+      })}
     </nav>
   );
 }
