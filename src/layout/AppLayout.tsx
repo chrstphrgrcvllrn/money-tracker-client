210d93365d5054f2f11fd5937017c8c16ffc6a42
christopher-villarin <christopher.villarin@hogarth.com>
2026-03-25 18:17:25 +0800

    huge update

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout() {
   return (
     <div className="flex">
       <BottomNavBar />
-      <main className="flex-1 bg-[#111111] p-0 h-[90vh] overflow-y-auto">
+      <main className="flex-1 bg-[#111111] p-0 h-[90svh] overflow-y-auto">
            <Outlet />
       </main>
     </div>
