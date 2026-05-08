ebd208414ef39079618843d3c6182b3559f99d1a
christopher-villarin <christopher.villarin@hogarth.com>
2026-05-09 00:27:41 +0800

    big change

--- src/api/subscription.ts
+++ src/api/subscription.ts
@@ -1,24 +1,28 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/subscription";
+
+const BASE_URL =
+  import.meta.env.NODE_ENV === "production"
+    ? `${import.meta.env.VITE_PROD_API_URL}/api/subscription`
+    : `${import.meta.env.VITE_DEV_API_URL}/api/subscription`
 
 export const getSubscriptions = async () => {
-  const res = await axios.get(API);
+  const res = await axios.get(BASE_URL);
   return res.data;
 };
 
 export const createSubscription = async (data: any) => {
-  const res = await axios.post(API, data);
+  const res = await axios.post(BASE_URL, data);
   return res.data;
 };
 
 export const updateSubscription = async (id: string, data: any) => {
-  const res = await axios.put(`${API}/${id}`, data);
+  const res = await axios.put(`${BASE_URL}/${id}`, data);
   return res.data;
 };
 
 export const deleteSubscription = async (id: string) => {
-  const res = await axios.delete(`${API}/${id}`);
+  const res = await axios.delete(`${BASE_URL}/${id}`);
   return res.data;
 };
 
@@ -29,6 +33,6 @@ export const updatePayment = async (data: {
   date?: string;
   status?: "paid" | "pending";
 }) => {
-  const res = await axios.patch(`${API}/payment`, data);
+  const res = await axios.patch(`${BASE_URL}/payment`, data);
   return res.data;
 };
